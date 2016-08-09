d7492532d160938e6662261b3dcec9807e2d822b
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-08-09 18:58:25 -0500

    fixed all major bugs

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-$(document).ready(function (){
+var Acme = (function (Acme) {
+  $(document).ready(function (){
 
 // This function does one thing, and returns a promise
 
@@ -40,9 +41,9 @@ var xhrButtons = function () {
     $('.fireworks').addClass('visible').removeClass('notshown')
     $('.demolition').addClass('notShown').removeClass('visible')
     $('.products').addClass('visible').removeClass('notShown')
-    showFireworks(products)
 
   })
+    Acme.showFireworks(products)
 
   $('.Demolition').click(function pickedDemolition() {
     console.log('demolition clicked')
@@ -52,9 +53,9 @@ var xhrButtons = function () {
     $('.demolition').addClass('visible').removeClass('notshown')
     $('.fireworks').addClass('notShown').removeClass('visible')
     $('.products').addClass('visible').removeClass('notShown')
-    showDemolition(products)
 
  })
+    Acme.showDemolition(products)
 
 }
 
@@ -68,67 +69,11 @@ $('.home').click(function() {
 
 /////////IDEALLY WILL GO IN A DIFFERENT JS FILE
 
-  var personalFireworks =" ";
-  var professionalFireworks = " ";
-  var noveltyFireworks = " ";
 
-
-var showFireworks = function(products) {
-  console.log('showFireworks starting', products)
-  for(key in products){
-    if (products[key].type === 0) {
-    console.log('working?',products[key].name)
-    personalFireworks +='<div class="productCard col-sm-4">';
-    personalFireworks += '<h3>' + products[key].name+ '</h3>'
-    personalFireworks +='<p>' + products[key].description+ '</p></div>'
-  } else if (products[key].type === 1) {
-    professionalFireworks +='<div class="productCard col-sm-4">';
-    professionalFireworks += '<h3>' + products[key].name+ '</h3>'
-    professionalFireworks +='<p>' + products[key].description+ '</p></div>'
-  } else if (products[key].type === 2) {
-    noveltyFireworks +='<div class="productCard col-sm-4">';
-    noveltyFireworks += '<h3>' + products[key].name+ '</h3>'
-    noveltyFireworks +='<p>' + products[key].description+ '</p></div>'
-  }
-  }
-  console.log(personalFireworks)
-  $('.forPersonal').html(personalFireworks)
-  $('.forProfessional').html(professionalFireworks)
-  $('.forNovelty').html(noveltyFireworks)
-}
-
-
-
-  var dynamite = '';
-  var implossion = '';
-  var explosion = '';
-
-var showDemolition = function(products) {
-  console.log('showDemolition starting', products)
-  for(key in products){
-    if (products[key].type === 3) {
-    console.log('working?',products[key].name)
-    dynamite +='<div class="productCard col-sm-4">';
-    dynamite += '<h3>' + products[key].name+ '</h3>'
-    dynamite +='<p>' + products[key].description+ '</p></div>'
-  } else if (products[key].type === 4) {
-    implossion +='<div class="productCard col-sm-4">';
-    implossion += '<h3>' + products[key].name+ '</h3>'
-    implossion +='<p>' + products[key].description+ '</p></div>'
-  } else if (products[key].type === 5) {
-    explosion +='<div class="productCard col-sm-4">';
-    explosion += '<h3>' + products[key].name+ '</h3>'
-    explosion +='<p>' + products[key].description+ '</p></div>'
-  }
-  }
-  console.log(dynamite)
-  $('.forDynamite').html(dynamite)
-  $('.forImplossion').html(implossion)
-  $('.forExplosion').html(explosion)
-
-}
 
 
 
 });
+return Acme
 //ending of the jQuery block
+})(Acme|| {})
--- show.js
+++ show.js
@@ -1,15 +1,68 @@
- $(document).ready(function (){
+
+ var Acme = (function(acmeStore){
+  $(document).ready(function (){
 
 
-var showDemolition = function(products) {
-    if (products[index].type === 3) {
-      console.log('works')
-    } else if (products[index].type === 4) {
+  var personalFireworks =" ";
+  var professionalFireworks = " ";
+  var noveltyFireworks = " ";
 
-    } else {
 
-    }
+acmeStore.showFireworks = function(products) {
+  console.log('showFireworks starting', products)
+  for(key in products){
+    if (products[key].type === 0) {
+    console.log('working?',products[key].name)
+    personalFireworks +='<div class="productCard col-sm-4">';
+    personalFireworks += '<h3>' + products[key].name+ '</h3>'
+    personalFireworks +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 1) {
+    professionalFireworks +='<div class="productCard col-sm-4">';
+    professionalFireworks += '<h3>' + products[key].name+ '</h3>'
+    professionalFireworks +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 2) {
+    noveltyFireworks +='<div class="productCard col-sm-4">';
+    noveltyFireworks += '<h3>' + products[key].name+ '</h3>'
+    noveltyFireworks +='<p>' + products[key].description+ '</p></div>'
+  }
+  }
+  console.log(personalFireworks)
+  $('.forPersonal').html(personalFireworks)
+  $('.forProfessional').html(professionalFireworks)
+  $('.forNovelty').html(noveltyFireworks)
+}
+
+
+
+  var dynamite = '';
+  var implossion = '';
+  var explosion = '';
+
+acmeStore.showDemolition = function(products) {
+  console.log('showDemolition starting', products)
+  for(key in products){
+    if (products[key].type === 3) {
+    console.log('working?',products[key].name)
+    dynamite +='<div class="productCard col-sm-4">';
+    dynamite += '<h3>' + products[key].name+ '</h3>'
+    dynamite +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 4) {
+    implossion +='<div class="productCard col-sm-4">';
+    implossion += '<h3>' + products[key].name+ '</h3>'
+    implossion +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 5) {
+    explosion +='<div class="productCard col-sm-4">';
+    explosion += '<h3>' + products[key].name+ '</h3>'
+    explosion +='<p>' + products[key].description+ '</p></div>'
+  }
+  }
+  console.log(dynamite)
+  $('.forDynamite').html(dynamite)
+  $('.forImplossion').html(implossion)
+  $('.forExplosion').html(explosion)
 
 }
 
 })
+  return acmeStore
+})(Acme || {})
