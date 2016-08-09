548203dde8d5a1280e6884af645a096213f1290b
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-08-09 15:43:14 -0500

    fireworks now works

--- scripts.js
+++ scripts.js
@@ -17,6 +17,7 @@ $( document ).ready(function() {
     categories =info[0].categories
     type = info[1].types
     products = info[2].products[0]
+    console.log(products)
   })
   .then( function(info) {
     console.log('making buttons')
@@ -26,6 +27,8 @@ $( document ).ready(function() {
     </ul>`
     $('.placeForCategories').html(choices);
   }).then(xhrButtons)
+
+    // .then(showDemolition)
 });
 
 
@@ -36,9 +39,10 @@ var xhrButtons = function () {
     $('.products').addClass('visible').removeClass('notShown')
     $('.fireworks').addClass('visible').removeClass('notshown')
     $('.demolition').addClass('notShown').removeClass('visible')
-    $('.products').addClass('visible').removeClass('notShown');
+    $('.products').addClass('visible').removeClass('notShown')
+    showFireworks(products)
 
-  })//.then(showFireworks)
+  })
 
   $('.Demolition').click(function pickedDemolition() {
     console.log('demolition clicked')
@@ -47,10 +51,10 @@ var xhrButtons = function () {
     $('.products').addClass('visible').removeClass('notShown')
     $('.demolition').addClass('visible').removeClass('notshown')
     $('.fireworks').addClass('notShown').removeClass('visible')
-    $('.products').addClass('visible').removeClass('notShown');
+    $('.products').addClass('visible').removeClass('notShown')
+    showFireworks(products)
 
-
- })//.then(showDemolition)
+ })
 
 }
 
@@ -60,5 +64,72 @@ $('.home').click(function() {
     $('.products').addClass('notShown').removeClass('visible');
     $().addClass('notShown')
   })
+
+
+/////////IDEALLY WILL GO IN A DIFFERENT JS FILE
+
+  var personalFireworks =" ";
+  var professionalFireworks = " ";
+  var noveltyFireworks = " ";
+
+
+var showFireworks = function(products) {
+  console.log('showFireworks starting', products)
+  for(key in products){
+    if (products[key].type === 0) {
+    console.log('working?',products[key].name)
+    personalFireworks +='<div class="productCard col-sm-4">';
+    personalFireworks += '<h3>' + products[key].name+ '</h3>'
+    personalFireworks +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 1) {
+    professionalFireworks +='<div class="productCard col-sm-4">';
+    professionalFireworks += '<h3>' + products[key].name+ '</h3>'
+    professionalFireworks +='<p>' + products[key].description+ '</p></div>'
+  } else if (products[key].type === 2) {
+    noveltyFireworks +='<div class="productCard col-sm-4">';
+    noveltyFireworks += '<h3>' + products[key].name+ '</h3>'
+    noveltyFireworks +='<p>' + products[key].description+ '</p></div>'
+  }
+  }
+  console.log(personalFireworks)
+  $('.forPersonal').html(personalFireworks)
+  $('.forProfessional').html(professionalFireworks)
+  $('.forNovelty').html(noveltyFireworks)
+}
+
+
+
+//   var dynatmie = $('forDynamite');
+//   var implossion = $('forImplossion');
+//   var explosion = $('forExplosion');
+
+// var showDemolition = function(products) {
+//     if (products[index].type === 3) {
+//       products.forEach (function () {
+//       dynatmie.append(
+//         `<div class="productCard">
+//         <h3>${products.name}</h3>
+//         <p>${products.description}</p></div>`)
+//       })
+//     } else if (products[index].type === 4) {
+//       products.forEach (function () {
+//       implossion.append(
+//         `<div class="productCard">
+//         <h3>${products.name}</h3>
+//         <p>${products.description}</p></div>`)
+//       })
+//     } else {
+//       products.forEach (function () {
+//       explosion.append(
+//         `<div class="productCard">
+//         <h3>${products.name}</h3>
+//         <p>${products.description}</p></div>`)
+//       })
+//     }
+
+// }
+
+
+
 });
 //ending of the jQuery block
--- show.js
+++ show.js
@@ -1,5 +1,15 @@
  $(document).ready(function (){
 
 
+var showDemolition = function(products) {
+    if (products[index].type === 3) {
+      console.log('works')
+    } else if (products[index].type === 4) {
+
+    } else {
+
+    }
+
+}
 
 })
